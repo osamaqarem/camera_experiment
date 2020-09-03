644f8ff9501f15413e8ba568e56853810c22feba
Osama <osamaqarem@gmail.com>
2020-09-03 11:14:06 +0800

    first commit

---
+++ pages/index.tsx
@@ -0,0 +1,36 @@
+import Head from "next/head"
+import { useEffect } from "react"
+
+export default function Home() {
+  const getMedia = async () => {
+    let stream = null
+
+    try {
+      stream = await navigator.mediaDevices.getUserMedia({
+        video: { width: 1280, height: 720 },
+      })
+      /* use the stream */
+    } catch (err) {
+      /* handle the error */
+    }
+  }
+
+  return (
+    <div>
+      <Head>
+        <title>Template</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className="p-10">
+        <button
+          className="bg-indigo-400 w-64 h-10 text-white text-xl rounded hover:bg-indigo-500"
+          onClick={getMedia}
+        >
+          Get media
+        </button>
+      </main>
+      <footer />
+    </div>
+  )
+}
