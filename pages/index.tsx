a6fa09135a36058497f6f934123e1ae12ea1a497
Osama <osamaqarem@gmail.com>
2020-09-03 14:19:13 +0800

    camera

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,41 @@
 import Head from "next/head"
+import { useRef } from "react"
 
 export default function Home() {
-  const getMedia = async () => {
-    let stream = null
+  const stream = useRef<MediaStream | null>(null)
+  const video = useRef<HTMLVideoElement | null>(null)
+  const canvas = useRef<HTMLCanvasElement | null>(null)
+  const img = useRef<HTMLImageElement | null>(null)
 
+  const getMedia = async () => {
     try {
-      stream = await navigator.mediaDevices.getUserMedia({
-        video: { width: 1280, height: 720 },
+      stream.current = await navigator.mediaDevices.getUserMedia({
+        video: { width: 1280, height: 720, facingMode: "user" },
       })
-      /* use the stream */
+      video.current!.srcObject = stream.current
+      video.current?.play()
     } catch (err) {
-      /* handle the error */
+      alert("getMedia error. Check console.")
+      console.error(err)
+    }
+  }
+
+  const takePicture = () => {
+    const context = canvas.current!.getContext("2d") as CanvasRenderingContext2D
+    if (video.current!.width && video.current!.height) {
+      canvas.current!.width = video.current!.width
+      canvas.current!.height = video.current!.height
+
+      context.drawImage(
+        video.current!,
+        0,
+        0,
+        video.current!.width,
+        video.current!.height
+      )
+
+      const data = canvas.current!.toDataURL("image/png")
+      img.current!.setAttribute("src", data)
     }
   }
 
@@ -22,12 +47,38 @@ export default function Home() {
       </Head>
 
       <main className="p-10">
-        <button
-          className="bg-indigo-400 w-64 h-10 text-white text-xl rounded hover:bg-indigo-500"
-          onClick={getMedia}
-        >
-          Get media
-        </button>
+        <div className="flex flex-col items-center">
+          {JSON.stringify("<video/>")}
+          <video
+            ref={video}
+            className="bg-purple-200"
+            width={1280}
+            height={720}
+          >
+            Video stream not available.
+          </video>
+          <div className="py-10" />
+          <div className="flex gap-x-10 justify-center">
+            <button
+              className="bg-indigo-400 w-40 h-10 text-white text-xl rounded hover:bg-indigo-500"
+              onClick={getMedia}
+            >
+              Get media
+            </button>
+            <button
+              className="bg-indigo-400 w-40 h-10 text-white text-xl rounded hover:bg-indigo-500"
+              onClick={takePicture}
+            >
+              Take photo
+            </button>
+          </div>
+          <div className="py-10" />
+
+          <canvas ref={canvas} className="hidden" />
+
+          {JSON.stringify("<img/>")}
+          <img ref={img} />
+        </div>
       </main>
       <footer />
     </div>
